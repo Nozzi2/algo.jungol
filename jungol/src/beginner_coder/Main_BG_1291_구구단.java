699c6ea6629d65117f6666d6c1d7b8215b98a8c7
Nozzi2 <zdsa43@gmail.com>
2022-07-26 22:52:38 +0900

    0726

---
+++ jungol/src/beginner_coder/Main_BG_1291_구구단.java
@@ -0,0 +1,46 @@
+package beginner_coder;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main_BG_1291_구구단 {
+	public static void main(String[] args) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in);
+		int start, end;
+
+		while (true) {
+//			String[] s = br.readLine().split(" ");
+//			start = Integer.parseInt(s[0]);
+//			end = Integer.parseInt(s[1]);
+			start = sc.nextInt();
+			end = sc.nextInt();
+			if (start > 9 || start < 2 || end > 9 || end < 2 || start == end) {
+				System.out.println("INPUT ERROR!");
+			} else {
+				break;
+			}
+		}
+
+		for (int i = 1; i < 10; i++) {
+			if (start > end) {
+				for (int j = start; j >= end; j--) {
+					System.out.printf("%d * %d = %2d", j, i, j*i);
+					if (j != end) {
+						System.out.print("   ");
+					}
+				}
+				System.out.println();
+			} else {
+				for (int j = start; j <= end; j++) {
+					System.out.printf("%d * %d = %2d", j, i, j*i);
+					if (j != end) {
+						System.out.print("   ");
+					}
+				}
+				System.out.println();
+			}
+		}
+
+	}
+}
